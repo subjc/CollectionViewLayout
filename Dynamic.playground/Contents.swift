ead9d4cb1749055fc4940c691ce322425c1b7c28
toshi0383 <t.suzuki326@gmail.com>
2017-10-14 19:15:38 +0900

    Change color from red to lightGray. It's better for your eyes

--- Dynamic.playground/Contents.swift
+++ Dynamic.playground/Contents.swift
@@ -239,7 +239,7 @@ class DataSource: NSObject, UICollectionViewDataSource {
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
-        cell.backgroundColor = .red
+        cell.backgroundColor = .lightGray
         return cell
     }
 }
